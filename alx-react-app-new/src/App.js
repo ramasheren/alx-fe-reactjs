6848f51e0b5e5bdd7f51efb4a3683427ea5cd4c6
Rama Sheren <ramasheren2004@gmail.com>
2025-07-18 20:35:19 +0300

    j

--- alx-react-app-new/src/App.js
+++ alx-react-app-new/src/App.js
@@ -1,15 +1,11 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import React from 'react';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <UserProfile name="Rama" age={20} bio="Aspiring software engineer and creative mind." />
-      <Footer />
+      <h1 style={{ textAlign: 'center' }}>Simple Counter App</h1>
+      <Counter />
     </div>
   );
 }
